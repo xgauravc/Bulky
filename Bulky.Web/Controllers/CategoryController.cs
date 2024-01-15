9b5c21c0841f8c4b2dc3169664cf0edcce026bf4
xgauravc <gauravchawre580@gmail.com>
2024-01-15 21:35:44 +0530

    Seeded the db , done with crud and added toastr notification

---
+++ Bulky.Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Bulky.Web.Data;
+using Bulky.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
